63b67ad5f475392964afdfdb6d06ce5798f1373b
1VanGuard1 <vanguard0109@gmail.com>
2025-11-25 11:59:41 +0300

    fix gitignore

--- Models/MotorOils.cs
+++ Models/MotorOils.cs
@@ -3,7 +3,7 @@
     public class MotorOils
     {
         public int ProductID { get; set; }
-        public double Viscosity { get; set; }
+        public double Viscosity { get; set; } // Вязкость
         public double Volume { get; set; }
         public required string OilType { get; set; }
         public required string ManufacturerCountry { get; set; }
